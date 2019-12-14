d25c2ed911423067a99bc08fb3c5cbc8c9d456f5
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2019-12-13 20:54:52 -0500

    Add solution for day 4 part 2

--- src/day4.lisp
+++ src/day4.lisp
@@ -55,3 +55,47 @@
   (loop
     :for n :from min :to max
     :count (valid-number-p n)))
+
+(defun group-adjacent (seq &key test)
+  (setf test (or test #'eql))
+  (let ((groups ())
+        (last-group nil))
+    (map nil (lambda (elt)
+               (cond
+                 ((or (null last-group)
+                      (funcall test elt (car last-group)))
+                  (push elt last-group))
+                 (t
+                  (push (nreverse last-group) groups)
+                  (setf last-group (list elt)))))
+         seq)
+    (when last-group
+      (push (nreverse last-group) groups))
+    (nreverse groups)))
+
+(defun adjacent-digits-same-v2-p (digits)
+   "Returns true if there is one pair of exactly two adjacent digits are equal in `digits'
+
+ (adjacent-digits-same-v2-p '(1 1 1) ; => NIL
+ (adjacent-digits-same-v2-p '(1 2 3) ; => NIL
+ (adjacent-digits-same-v2-p '(1 1 1 2 2)) ; => T
+"
+  (loop
+    :for group :in (group-adjacent digits)
+      :thereis (= (list-length group) 2)))
+
+(defun valid-number-v2-p (n)
+  "Returns true if `n' is a valid password number as per part2 rules."
+  (let ((digits (split-digits n)))
+    (and (= (list-length digits) 6)
+         (adjacent-digits-same-v2-p digits)
+         (non-decreasing-p digits))))
+
+(defun part2 (&optional (min 235741) (max 706948))
+  (when (or (< 999999 min) (< max 99999))
+    (return-from part2 0))
+  (unless (<= min max)
+    (error "min must be below max"))
+  (loop
+    :for n :from min :to max
+    :count (valid-number-v2-p n)))
